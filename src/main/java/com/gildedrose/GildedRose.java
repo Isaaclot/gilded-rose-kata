627d8a9907a8501dc08be993406002bf13ffcbf4
lqian <liangqian0723@gmail.com>
2020-05-24 18:30:17 +0800

    迁移方法 feature envy

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,75 +12,10 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            countSellIn(item);
-            countQuality(item);
+            item.countSellIn();
+            item.countQuality(this);
         }
     }
 
-    private void countQuality(Item item) {
-        if (item.name.equals(BACKSTAGE_PASSES)) {
-            countBackPassQuality(item);
-        } else if (item.name.equals(AGED_BRIE)) {
-            countAgedBrieQuality(item);
-        } else if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            countSalfurasQuality();
-        } else {
-            countCommonItemQuality(item);
-        }
-    }
 
-    private void countSalfurasQuality() {
-        // Sulfuras, Hand of Ragnaros empty method
-    }
-
-    private void countCommonItemQuality(Item item) {
-        if (item.quality > 0) {
-            decrQuality(item, 1);
-            if (item.sellIn < 0) {
-                decrQuality(item, 1);
-            }
-        }
-    }
-
-    private void countAgedBrieQuality(Item item) {
-        if (item.quality < 50) {
-            incrQuality(item, 1);
-        }
-        if (item.sellIn < 0 && item.quality < 50) {
-            incrQuality(item, 1);
-        }
-    }
-
-    private void countBackPassQuality(Item item) {
-        if (item.quality < 50) {
-            incrQuality(item, 1);
-        }
-        if (item.sellIn < 11) {
-            if (item.quality < 50) {
-                incrQuality(item, 1);
-            }
-        }
-        if (item.sellIn < 6) {
-            if (item.quality < 50) {
-                incrQuality(item, 1);
-            }
-        }
-        if (item.sellIn < 0) {
-            decrQuality(item, item.quality);
-        }
-    }
-
-    private void incrQuality(Item item, int offSet) {
-        item.quality = item.quality + offSet;
-    }
-
-    private void decrQuality(Item item, int offSet) {
-        item.quality = item.quality - offSet;
-    }
-
-    private void countSellIn(Item item) {
-        if (!item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            item.sellIn = item.sellIn - 1;
-        }
-    }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,71 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    void countSellIn() {
+        if (!name.equals(GildedRose.SULFURAS_HAND_OF_RAGNAROS)) {
+            sellIn = sellIn - 1;
+        }
+    }
+
+    public void countQuality(GildedRose gildedRose) {
+        if (name.equals(GildedRose.BACKSTAGE_PASSES)) {
+            countBackPassQuality(gildedRose);
+        } else if (name.equals(GildedRose.AGED_BRIE)) {
+            countAgedBrieQuality(gildedRose);
+        } else if (name.equals(GildedRose.SULFURAS_HAND_OF_RAGNAROS)) {
+            countSalfurasQuality();
+        } else {
+            countCommonItemQuality(gildedRose);
+        }
+    }
+
+    public void countCommonItemQuality(GildedRose gildedRose) {
+        if (quality > 0) {
+            decrQuality(1);
+            if (sellIn < 0) {
+                decrQuality(1);
+            }
+        }
+    }
+
+    public void countAgedBrieQuality(GildedRose gildedRose) {
+        if (quality < 50) {
+            incrQuality(1);
+        }
+        if (sellIn < 0 && quality < 50) {
+            incrQuality(1);
+        }
+    }
+
+    public void countBackPassQuality(GildedRose gildedRose) {
+        if (quality < 50) {
+            incrQuality(1);
+        }
+        if (sellIn < 11) {
+            if (quality < 50) {
+                incrQuality(1);
+            }
+        }
+        if (sellIn < 6) {
+            if (quality < 50) {
+                incrQuality(1);
+            }
+        }
+        if (sellIn < 0) {
+            decrQuality(quality);
+        }
+    }
+
+    public void incrQuality(int offSet) {
+        quality = quality + offSet;
+    }
+
+    public void decrQuality(int offSet) {
+        quality = quality - offSet;
+    }
+
+    public static void countSalfurasQuality() {
+        // Sulfuras, Hand of Ragnaros empty method
+    }
 }
